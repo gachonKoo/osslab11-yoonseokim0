572042635f710efb392775b08aae85f1a7c2d41e
unknown <rladbstj015@naver.com>
2025-11-14 13:33:55 +0900

    Complete utils and tester

---
+++ geo/utils.py
@@ -0,0 +1,9 @@
+def pythagoras(a, b):
+    """직각삼각형 빗변 계산"""
+    return (a**2 + b**2)**0.5
+
+def circle(radius):
+    """원의 넓이 계산"""
+    import math
+    return math.pi * radius**2
+
---
+++ tester.py
@@ -0,0 +1,4 @@
+import geo.utils as utils
+
+print("pythagoras(3,4) =", utils.pythagoras(3,4))
+print("circle(5) =", utils.circle(5))
