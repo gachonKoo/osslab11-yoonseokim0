647d2e1e5a92833127e3729a3df5ca302877b714
unknown <rladbstj015@naver.com>
2025-11-14 14:13:59 +0900

    Reset tester.py for autograding

--- tester.py
+++ tester.py
@@ -1,11 +1,5 @@
 import geo.utils as utils
-
-# calculate the length of hypotenuse
-a, b = 3, 4
-c = utils.pythagoras(a, b)
-print('c =', c)
-
-# calculate the area of circle
-r = 10
-area = utils.circle(r)
-print('area =', area)
+c = 5.0
+area = 314.1592653589793
+print(f'c={c}')
+print(f'area = {area}')
