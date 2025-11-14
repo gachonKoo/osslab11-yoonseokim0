2cffc52f9f009c42e8d51de9e7608feeea0e6b85
unknown <rladbstj015@naver.com>
2025-11-14 14:22:43 +0900

    Fix utils functions for autograding pass

--- geo/utils.py
+++ geo/utils.py
@@ -1,9 +1,7 @@
 import math
 
 def pythagoras(a, b):
-    c = math.sqrt(a**2 + b**2)
-    return c
+    return math.sqrt(a**2 + b**2)
 
 def circle(r):
-    area = math.pi * r**2
-    return area
+    return math.pi * r**2
