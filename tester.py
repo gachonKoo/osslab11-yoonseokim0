6aa3af5c8052417eef7e9834a8bd9000bab22c5a
unknown <rladbstj015@naver.com>
2025-11-14 14:46:46 +0900

    Fix utils and tester.py for autograding pass

--- tester.py
+++ tester.py
@@ -1,11 +1,11 @@
 import geo.utils as utils
 
-a = 3
-b = 4
-r = 10
-
+# calculate the length of hypotenuse(c) when a=3 and b=4
+a, b = 3, 4
 c = utils.pythagoras(a, b)
-area = utils.circle(r)
+print('c =', c)
 
-print("c = {}".format(c))
-print("area = {}".format(area))
+# calculate the area of circle with radius r = 10
+r = 10
+area = utils.circle(r)
+print('area =', area)
