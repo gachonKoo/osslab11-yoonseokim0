65fd9ca58dbc66a356d124d20310b106b4c03730
unknown <rladbstj015@naver.com>
2025-11-14 14:31:35 +0900

    Fix utils and tester.py formatting for autograding pass

--- tester.py
+++ tester.py
@@ -4,15 +4,8 @@ a = 3
 b = 4
 r = 5
 
-import geo.utils as utils
-
-a = 3
-b = 4
-r = 5
-
 c = utils.pythagoras(a, b)
 area = utils.circle(r)
 
 print("c = {}".format(c))
 print("area = {}".format(area))
-
